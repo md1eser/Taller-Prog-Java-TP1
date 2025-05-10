646024c3345334f55b383d4225b05bf125c6a4b6
Martin Dieser <martindieser14@gmail.com>
2025-05-09 23:00:50 -0300

    Initial commit

---
+++ src/main/java/exa/unicen/tallerprogjava/App.java
@@ -0,0 +1,13 @@
+package exa.unicen.tallerprogjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
